d2e465ed3b78f4da6c07c1bdad4d97ff174627d0
Will Sams <will@samswebs.com>
2021-09-04 14:50:22 -0400

    Jumping back on the horse after 4 years.

--- demos/common.mk
+++ demos/common.mk
@@ -1,12 +1,10 @@
-#cc65 is in my $PATH.  If it isn't in yours, adjust for your system
+#!/bin/sh
+
 AS	= ca65
 LD	= ld65 
 LDFLAGS = -C nes.ld -m $(ROM).map
 OBJDUMP = od65
-
-#put whatever emulator you use here.  Mednafen is in my $PATH, so...
-DEBUGGER = mednafen
-
+DEBUGGER = fceux
 BIN	= $(ROM).nes
 
 all:	$(BIN)
@@ -24,11 +22,5 @@ dump:
 	$(OBJDUMP) --dump-all $(OBJS) > $(ROM).dump
 
 run:
-	echo 'Mednafen - Hold Alt-D to enter debugger.' 
-	echo 'Press any key to continue to execute the rom' 
-	$(DEBUGGER) -nes.enable 1 $(BIN) 
-
-#I'm on Linux, so I moved fceux Windows version in my projects path.  Linux version doesn't have debugger
-debug:
-	~/Projects/nes/a_tools/fceuxw/fceux.exe ./$(BIN)
+	$(DEBUGGER) ./$(BIN) 
 
